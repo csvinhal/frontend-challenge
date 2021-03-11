09c3292cbc6269bab04b408a24445959fce78a70
csvinhal <cristiano.vinhal10@gmail.com>
2021-03-11 19:10:21 -0300

    added storybook to project

---
+++ my-code/.storybook/preview.js
@@ -0,0 +1,14 @@
+import '../pages/styles.css'
+
+export const parameters = {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    backgrounds: {
+        default: 'Dark',
+        values: [
+            {
+                name: 'Dark',
+                value: '#0a1014',
+            },
+        ],
+    },
+}
