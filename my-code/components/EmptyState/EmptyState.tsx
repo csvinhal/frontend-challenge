132e143ecb02ddbf4e7c9b069d185b0aff953ca6
cristiano_vinhal <cristiano.vinhal@br.experian.com>
2021-03-13 23:14:22 -0300

    create EmptyState component

---
+++ my-code/components/EmptyState/EmptyState.tsx
@@ -0,0 +1,42 @@
+import Typography from '../Typography/Typography'
+
+const EmptyState = () => {
+    return (
+        <div className="empty-state">
+            <img
+                className="empty-state__img"
+                src="/illustration-empty-state.png"
+                alt="Empty state image"
+                srcSet={`/illustration-empty-state@2x.png 2x`}
+                width={396}
+                height={193}
+            />
+            <Typography
+                className="empty-state__title"
+                size="medium"
+                weight="medium"
+            >
+                Don't know what to search?
+            </Typography>
+            <Typography color="secundary">Here's an offer you can't refuse</Typography>
+            <style jsx>{`
+                .empty-state {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    flex-direction: column;
+                }
+
+                .empty-state__img {
+                    margin-bottom: 16px;
+                }
+
+                .empty-state :global(.empty-state__title) {
+                    margin-bottom: 8px;
+                }
+            `}</style>
+        </div>
+    )
+}
+
+export default EmptyState
---
+++ my-code/stories/EmptyState.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, Story } from '@storybook/react/types-6-0'
+import { ComponentProps } from 'react'
+import EmptyState from '../components/EmptyState/EmptyState'
+
+export default {
+    title: 'Empty State',
+    component: EmptyState,
+} as Meta
+
+const Template: Story<ComponentProps<typeof EmptyState>> = ({ ...args }) => (
+    <EmptyState {...args} />
+)
+
+export const Default = Template.bind({})
